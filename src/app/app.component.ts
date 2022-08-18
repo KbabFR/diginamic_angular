4401db867953f3a0adf254b54940ddac00ef7354
KbabFR <43203741+KbabFR@users.noreply.github.com>
2022-08-18 11:48:05 +0200

    project without users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'users_todolist_project';
+
+  filter: 'all' | 'vital' | 'fun' | 'work' = 'all';
+
+  allItems = [
+    { description: 'Eat', done: true , category: 'vital'},
+    { description: 'Sleep', done: false , category: 'vital'},
+    { description: 'Play', done: false , category: 'fun'},
+    { description: 'Laugh', done: false , category: 'fun'},
+    { description: 'Code', done: false , category: 'work'},
+  ];
+
+  get filteredItems() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === item.category);
+  }
+
+  get itemsActive() {
+    return this.filteredItems.filter((item) => !item.done);
+  }
+
+  get itemsDone() {
+    return this.filteredItems.filter((item) =>item.done);
+  }
+
+  addItem(description: string, category: string) {
+    this.allItems.unshift({
+      description,
+      done: false,
+      category
+    });
+  }
+  
+  remove(item:Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+  
 }
